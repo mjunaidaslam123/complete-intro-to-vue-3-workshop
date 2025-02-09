e30039889b08ad02e9bf021593de8b8db4512a0d
mjunaidaslam123 <2020me385@student.uet.edu.pk>
2025-02-09 16:20:10 +0500

    feature: add Counter component with increment functionality and integrate into App.vue

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -28,11 +28,17 @@
       </p>
 
     </div>
+    <hr />
+    <Counter />
   </main>
 </template>
 
 <script>
+import Counter from '@/components/Counter.vue';
 export default {
+  components: {
+    Counter,
+  },
   data: () => ({
     chartype: [],
     newCharacter: {
